e9b7f269e7f2229c5d86a133980fa61306d37e22
Pablo Orozco Montes <pablo@commm.co>
2020-03-28 19:50:17 -0500

    Create single page

---
+++ pages/[uid].js
@@ -0,0 +1,11 @@
+import Layout from '../components/organism/Layout'
+
+export default class Home extends React.Component {
+    render() {
+        return(
+            <Layout>
+                
+            </Layout>
+        ) 
+    }
+}
